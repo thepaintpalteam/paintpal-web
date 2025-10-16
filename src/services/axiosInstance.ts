2fc83a268d54192bb1e3c0e00dfdec7018277ce8
Adejosh01 <adelobajoshua19@gmail.com>
2025-10-16 16:30:07 +0100

    fix

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://paint.bugreaper.link/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },

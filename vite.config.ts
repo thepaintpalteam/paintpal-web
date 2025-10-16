5154506d12512736b48ee4adab0c2e2a6847e6e9
Adejosh01 <adelobajoshua19@gmail.com>
2025-10-16 21:22:27 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ important for Hostinger
   server: {
     port: 7770,
   },
